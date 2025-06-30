5d56e8733af9a822fab5cf9f76bd1494f4adee59
jalscorecard <scorecard@jalsmruti.org>
2025-06-30 13:12:02 +0530

    map link

--- frontend/src/pages/Page3.jsx
+++ frontend/src/pages/Page3.jsx
@@ -24,13 +24,7 @@ function Page3({ onNext, onBack, form }) {
   const validateField = (name, value) => {
     switch (name) {
       case "mapLink":
-        if (
-          !/^https?:\/\/(www\.)?(google\.com\/maps|goo\.gl\/maps|maps\.app\.goo\.gl)/.test(
-            value
-          ) &&
-          !/^https?:\/\/google\.com\/maps/.test(value) &&
-          !/^https?:\/\/maps\.app\.goo\.gl/.test(value)
-        ) {
+        if (value && !/^https?:\/\/(www\.)?(google\.com\/maps|goo\.gl\/maps|maps\.app\.goo\.gl)/.test(value)) {
           return "Please enter a valid Google Maps link";
         }
         return "";
@@ -43,8 +37,9 @@ function Page3({ onNext, onBack, form }) {
     e.preventDefault();
     let err = {};
     if (!data.apartmentName) err.apartmentName = "Required";
-    if (!data.mapLink || validateField("mapLink", data.mapLink))
-      err.mapLink = validateField("mapLink", data.mapLink) || "Required";
+    if (data.mapLink && validateField("mapLink", data.mapLink)) {
+      err.mapLink = validateField("mapLink", data.mapLink);
+    }
     if (!data.units || !/^\d+$/.test(data.units)) err.units = "Required";
     setError(err);
     if (Object.keys(err).length === 0) onNext(data);
@@ -78,7 +73,7 @@ function Page3({ onNext, onBack, form }) {
             required
           />
           <TextField
-            label="Google map location link of the Apartment Building being assessed *"
+            label="Google map location link of the Apartment Building being assessed"
             name="mapLink"
             value={data.mapLink}
             onChange={handleChange}
@@ -86,7 +81,6 @@ function Page3({ onNext, onBack, form }) {
             helperText={error.mapLink}
             fullWidth
             margin="normal"
-            required
           />
           <TextField
             label="How many Units/Flats in the Apartment Building *"
